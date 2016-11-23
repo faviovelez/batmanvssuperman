cc9c5bdef5ac8755f6db5152b941172246040ccd
FAVIO VELEZ <favio.velez@hotmail.com>
2016-11-22 21:15:14 -0600

    puntos 20 y 21 sin vista parcial

--- batmanvssuperman/config/routes.rb
+++ batmanvssuperman/config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
+  root 'pages#batman_vs_superman'
+
+  get 'pages/batman_vs_superman'
+
   get 'experiments/page1'
 
   get 'experiments/page2'
 
   get 'experiments/page3'
-
-  root 'pages#batman_vs_superman'
-
-  get 'pages/batman_vs_superman'
 
   post 'pages/batman_vs_superman_post'
 
